f79fdb6f6d3b40646a277b9a2f0f57df9ef513b2
aquairus <18650013469@163.com>
2016-05-22 15:45:09 +0800

    save

--- fabfile.py
+++ fabfile.py
@@ -12,3 +12,8 @@ def save():
 		local("git commit -m 'save' ")
 		local("git push")
 
+def dep():
+	local("git add -A")
+	with settings(warn_only=True):
+		local("git checkout master")
+		local("git merge ProfessorPaul")
