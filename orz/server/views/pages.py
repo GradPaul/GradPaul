9a6cd0849f673ea3b40d869cd6ae23aabded5d32
aquairus <18650013469@163.com>
2016-06-13 01:07:04 +0800

    save

--- orz/server/views/pages.py
+++ orz/server/views/pages.py
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+from flask import Blueprint, request, render_template
+from flask import current_app as app
+# import requests
+from server.utils import *
+
+pages = Blueprint(__name__, __name__)
+
+@pages.route("/")
+def index():
+    return render_template("index.html")
+
+@pages.route("/result")
+def result():
+    return render_template("result.html")
+
+@pages.route("/Professor")
+def Professor():
+    return render_template("/Professor/index.html")
+=======
 from flask import Blueprint, request, render_template
 from flask import current_app as app
 # import requests
@@ -19,3 +39,4 @@ def Professor():
 @pages.route("/Professor/comment")
 def comment():
     return render_template("/Professor/comment.html")
+>>>>>>> 790c97ba486e2e063df55c5169d585793ccefb21
